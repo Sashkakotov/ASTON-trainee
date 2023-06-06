9b5af7ece4d55140949379f6d7e5fde37eb9952c
Sashkakotov <sashkakotov@mail.ru>
2023-06-06 09:52:31 +0300

    feat: wrongArrays

---
+++ wrongArrays.js
@@ -0,0 +1,4 @@
+//TODO Why arrays in Javascript are wrong?
+
+//Arrays in Javascript are wrong, because unlike classical arrays, have dynamic length and includes data different types. 
+//Arrays in Javascript inherited from object and includes stack and queue.
